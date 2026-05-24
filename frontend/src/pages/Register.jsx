231dbca14e96a7a2f80923544908d731b108dfcd
Atharv Jambhule <atharvjambhule7@gmail.com>
2026-05-25 01:44:51 +0530

    Show actual registration error

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,60 +1,61 @@
 import { useState } from "react";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import API from "../services/api";
 
-function Register(){
+function Register() {
 
-const navigate=useNavigate();
+const navigate = useNavigate();
 
-const [form,setForm]=useState({
-
-name:"",
-email:"",
-password:""
-
+const [form, setForm] = useState({
+name: "",
+email: "",
+password: ""
 });
 
-
-const handleSubmit=async(e)=>{
+const handleSubmit = async (e) => {
 
 e.preventDefault();
 
-try{
+try {
 
-const res=await API.post(
-
+const res = await API.post(
 "/auth/register",
-
 form
-
 );
+
+console.log("SUCCESS:",res.data);
 
 localStorage.setItem(
-
 "token",
 res.data.token
-
 );
 
-navigate(
-"/dashboard"
-);
+navigate("/dashboard");
 
 }
 
 catch(error){
 
-console.log(error);
+console.log(
+"FULL ERROR:",
+error
+);
+
+console.log(
+"RESPONSE:",
+error.response?.data
+);
 
 alert(
-"Registration failed"
+
+error.response?.data?.msg ||
+"Registration Failed"
+
 );
 
 }
 
 };
-
-
 
 return(
 
@@ -62,96 +63,51 @@ return(
 
 <div className="auth-card">
 
-<h1>
-
-Register 🚀
-
-</h1>
+<h1>Register 🚀</h1>
 
 <form onSubmit={handleSubmit}>
 
 <input
-
-className="auth-input"
-
 placeholder="Name"
-
 value={form.name}
-
 onChange={(e)=>
-
 setForm({
-
 ...form,
 name:e.target.value
-
 })
-
 }
-
 />
 
-
 <input
-
-className="auth-input"
-
 placeholder="Email"
-
 value={form.email}
-
 onChange={(e)=>
-
 setForm({
-
 ...form,
 email:e.target.value
-
 })
-
 }
-
 />
-
 
 <input
-
-className="auth-input"
-
 type="password"
-
 placeholder="Password"
-
 value={form.password}
-
 onChange={(e)=>
-
 setForm({
-
 ...form,
 password:e.target.value
-
 })
-
 }
-
 />
 
-
-<button
-
-className="auth-btn"
-
-type="submit"
-
->
+<button type="submit">
 
 Register
 
 </button>
 
 </form>
-
 
 <p>
 
@@ -165,12 +121,11 @@ Login
 
 </p>
 
-
 </div>
 
 </div>
 
-)
+);
 
 }
 
