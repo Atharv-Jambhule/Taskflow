c3217a967079128f110091090b5fa49598329396
Atharv Jambhule <atharvjambhule7@gmail.com>
2026-05-25 01:09:52 +0530

    Fixed routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
-import {
-BrowserRouter,
-Routes,
-Route
-}
-from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
-function App(){
+function App() {
 
-return(
+return (
 
 <BrowserRouter>
 
